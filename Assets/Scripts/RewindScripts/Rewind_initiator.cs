50875c7fb6b3a62e09030bd3c8ef2080d9f58007
Bosssmann810 <sealered@gmail.com>
2026-02-17 11:27:16 -0400

    it works with multiple objects and reverses the rotation... as a wise man once said "ah yeah, it's all coming together"

--- Assets/Scripts/RewindScripts/Rewind_initiator.cs
+++ Assets/Scripts/RewindScripts/Rewind_initiator.cs
@@ -9,6 +9,7 @@ using UnityEngine.Assertions.Must;
 public class Rewind_initiator : MonoBehaviour
 {
     public List<Vector3> rewindPositions = new List<Vector3>();
+    public List<Quaternion> rewindRotations = new List<Quaternion>();
     public bool rewinding;
     public GameObject body;
     public Transform currentPos;
@@ -26,7 +27,7 @@ public class Rewind_initiator : MonoBehaviour
         //if we are not rewinding
         if(rewinding == false)
         {
-            Debug.Log(currentPos.position);
+            Debug.Log(currentPos.rotation);
             //and if the current position has changed
             if (rewindPositions.Contains(currentPos.position))
             {
@@ -35,6 +36,7 @@ public class Rewind_initiator : MonoBehaviour
             }
             //add the current position to the rewind positions
             rewindPositions.Add(currentPos.position);
+            rewindRotations.Add(currentPos.rotation);
         }
         //when rewinding is true
         if (rewinding == true)
@@ -50,7 +52,19 @@ public class Rewind_initiator : MonoBehaviour
             body.transform.position = rewindPositions.ElementAt(0);
             //then remove the element at the top
             rewindPositions.Remove(currentPos.position);
-            
+
+            //litterally the same thing as positions but with rotations
+            if (rewindRotations.ElementAt(0) == null)
+            {
+                return;
+            }
+            //find out the current rotation at element 0 of the list
+            currentPos.rotation = rewindRotations.ElementAt(0);
+            //set the body's transform to the current top element
+            body.transform.rotation = rewindRotations.ElementAt(0);
+            //then remove the element at the top
+            rewindRotations.RemoveAt(0);
+
         }
         
     }
@@ -59,6 +73,7 @@ public class Rewind_initiator : MonoBehaviour
         //when the button is pressed
             //reverse the list
         rewindPositions.Reverse();
+        rewindRotations.Reverse();
             //and begin rewinding
         rewinding = true;
             //also this is where I could add the funky effects.
@@ -68,6 +83,7 @@ public class Rewind_initiator : MonoBehaviour
 
     public void EndRewind()
     {
+        rewindRotations.Reverse();
         rewindPositions.Reverse();
         rewinding = false;
     }
